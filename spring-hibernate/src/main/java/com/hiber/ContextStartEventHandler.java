9813c5aa7acb93b63e4818c3b5131f9c50d6980d
tungtt93 <zhangqingsong21293@gmail.com>
2016-09-02 19:43:29 +0700

    up hibernate project

---
+++ spring-hibernate/src/main/java/com/hiber/ContextStartEventHandler.java
@@ -0,0 +1,29 @@
+///***************************************************************************
+// * 							tungtt							               *    
+// **************************************************************************/
+//package com.hiber;
+//
+//import javax.activation.DataSource;
+//
+//import org.apache.log4j.Logger;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.ApplicationListener;
+//import org.springframework.context.event.ContextStartedEvent;
+//
+///**
+// *  Author : tungtt         
+// * Aug 28, 2016
+// */
+//public class ContextStartEventHandler implements ApplicationListener<ContextStartedEvent> {
+//
+//	private final static Logger logger = Logger.getLogger(ContextStartEventHandler.class);
+//	
+//	@Autowired
+//	private DataSource dataSource;
+//	
+//	@Override
+//	public void onApplicationEvent(ContextStartedEvent arg0) {
+//		logger.info("context start application: " + dataSource);
+//	}
+//
+//}
