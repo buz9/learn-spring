068ee9dcec2a0b9fe53788964c3d32955c6f7894
tungtt93 <zhangqingsong21293@gmail.com>
2016-09-02 08:49:14 +0700

    up

--- spring-hibernate/src/main/java/com/hiber/ServletInitializer.java
+++ spring-hibernate/src/main/java/com/hiber/ServletInitializer.java
@@ -1,25 +1,13 @@
 package com.hiber;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
-@ImportResource("classpath:config.xml")
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SpringHibernateApplication.class);
-	}
-
-	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(ServletInitializer.class, args);
-		ctx.start();
 	}
 	
 }
--- spring-hibernate/src/main/java/com/hiber/SpringHibernateApplication.java
+++ spring-hibernate/src/main/java/com/hiber/SpringHibernateApplication.java
@@ -2,11 +2,16 @@ package com.hiber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:config.xml")
 public class SpringHibernateApplication {
 
 	public static void main(String[] args) {
+//		ConfigurableApplicationContext ctx = SpringApplication.run(ServletInitializer.class, args);
+//		ctx.start();
 		SpringApplication.run(SpringHibernateApplication.class, args);
 	}
+	
 }
